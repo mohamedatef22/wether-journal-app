c1bc7646502f247e4a404890da4aed14486582ce
Mohamed Atef <34000758+mohamedatef22@users.noreply.github.com>
2020-06-11 20:39:12 +0200

    run server

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+projectData = [];
+const port = 8000;
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
 
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+app.listen(port , ()=> {
+    console.log(`server running on port : ${port}`);
+});
+
+
+app.post('/add' , postRequest);
+// app.get('/all' , getRequest);
+
+function postRequest(req, res){
+    const newData = {date:req.body.date , zip:req.body.zip , feeling:req.body.feeling};
+    projectData.push(newData);
+    console.log(projectData);    
+};
+
+
+function getRequest(req , res){
+    console.log('getrec')
+    res.send(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -3,3 +3,54 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
+const key = '&appid=49652684bf08ec0131207130678a15be';
+
+console.log(newDate);
+
+
+
+const postData = async (url = '', data = {})=>{
+    const response = await fetch(url ,{
+        method:'POST',
+        credentials:'same-origin',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(data),
+    });
+    try{
+        console.log(data);
+        const res = await response.json();
+        return res;
+    }
+    catch(error){
+        console.log(error);
+    }
+       
+};
+
+document.getElementById('generate').addEventListener('click', postDataAction);
+
+function postDataAction(){
+    const zipDiv = document.getElementById('zip');
+    const feelingsDiv = document.getElementById('feelings');
+    postData('/add' , {date:newDate , zip:zipDiv.value , feeling:feelingsDiv.value})
+    .then(getData()); 
+}
+
+const getData = async ()=>{
+    const zipDiv = document.getElementById('zip');
+    const urlf = baseUrl + zipDiv.value + key;
+    console.log(urlf);
+    const re  = await fetch(urlf);
+    try{
+        const data = await re.json();
+        console.log(re.json());
+    }
+    catch(error){
+
+    }
+}
+
+getData(); 
